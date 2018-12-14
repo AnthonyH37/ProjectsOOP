1dc796059082f5e5b35e40cae0cfaa1918d9244a
AnthonyH37 <ahorvath0816@eagle.fgcu.edu>
2018-12-14 15:53:15 -0500

    Update EmployeeInfo.java

--- EmployeeInfo.java
+++ EmployeeInfo.java
@@ -15,8 +15,8 @@ public class EmployeeInfo {
     private String deptId;
     private Pattern p = Pattern.compile("^[A-Z][a-z]{3}[1-9]{2}$");
 
-
     EmployeeInfo() {
+
         setName();
         setDeptId();
     }
@@ -30,23 +30,24 @@ public class EmployeeInfo {
     }
 
     //The setName method is called from the constructor which will use inputName to get
-    //a full employee name as input.
-    
+    //a full employee name as input.    
+
     private void setName() {
         StringBuilder testName = new StringBuilder(inputName());
+
         if (checkName(testName)) {
             this.name = testName;
-            System.out.println("Valid name, creating employee code");
             createEmployeeCode(testName);
+
         } else {
             this.name = testName;
-            System.out.println("Invalid name, setting code to guest");
             this.code = "guest";
         }
     }
 
     //createEmployeeCode takes the first initial from the first
     //name and add it to the full surname to create the code
+
     private void createEmployeeCode(StringBuilder name) {
          this.code = name.charAt(0)+ name.substring(name.indexOf(" ")+1);
     }
@@ -54,12 +55,15 @@ public class EmployeeInfo {
     private String inputName() {
         System.out.println("Please enter your first and last name:");
         return in.nextLine();
+
     }
 
     //check if the name has a space in it
+
     private boolean checkName(StringBuilder name) {
         if (name.indexOf(" ") > 0) {
             return (name.indexOf(" ", name.indexOf(" ")+1) == -1);
+
         } else
             return false;
     }
@@ -71,41 +75,48 @@ public class EmployeeInfo {
 
     //setDeptId calls getDeptId to retrieve ID from user before validId is used to check the input
     //If the pattern matches, ID is set to deptId or a default value of None01 will be assigned
+
     private void setDeptId(){
         String id = getDeptId();
+
         if (validId(id)){
+
             this.deptId = id;
-            System.out.println("Department ID set.");
         }
         else{
-            System.out.println("Inadequate Department ID, setting to default.");
+
             this.deptId = "None01";
         }
-
-
     }
+
     private String getId(){
+
         return this.deptId;
+
     }
 
     //uses a matcher to test if ID matches the pattern
+
     private boolean validId(String id){
         Matcher matcher = p.matcher(id);
         return matcher.matches();
+
     }
 
     private String reverseString(String id){
+
         if(id.isEmpty()){
             return id;
+
         } else {
             return reverseString(id.substring(1))+id.charAt(0);
         }
     }
 
     @Override
+
     public String toString(){
         return "Employee Code: " + getCode() + "\n" +
                 "Department Number: " + getId();
     }
-
 }
